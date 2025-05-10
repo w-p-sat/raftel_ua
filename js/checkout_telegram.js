e96a81003e0b5d8898620b066e7a427d6229a006
Ruzke <146598189+w-p-sat@users.noreply.github.com>
2025-05-10 11:06:42 +0300

    Update checkout_telegram.js

--- js/checkout_telegram.js
+++ js/checkout_telegram.js
@@ -36,3 +36,20 @@ document.getElementById('send-to-telegram').addEventListener('click', function (
 
     window.location.href = telegramURL;
 });
+
+
+
+document.getElementById('send-to-telegram').addEventListener('click', () => {
+    const botToken = '8194429089:AAGZst6Z6zr0IncIpx5v0K7U3LSWy7f_RjQ';
+    const chatId = '5148446547';
+    const message = 'Користувач натиснув кнопку "Підтвердити Замовлення в Telegram"';
+
+    fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log('Відповідь Telegram:', data);
+      })
+      .catch(error => {
+        console.error('Помилка:', error);
+      });
+  });
