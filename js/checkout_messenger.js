9c841180be118cd33d1f1c90f0b5b5ca08efa710
Ruzke <146598189+w-p-sat@users.noreply.github.com>
2025-05-10 11:05:51 +0300

    Update checkout_messenger.js

--- js/checkout_messenger.js
+++ js/checkout_messenger.js
@@ -33,3 +33,19 @@ document.getElementById('send-to-messenger').addEventListener('click', function 
     // Перехід на сторінку Facebook Messenger
     window.location.href = messengerURL;
 });
+
+
+document.getElementById('send-to-messenger').addEventListener('click', () => {
+    const botToken = '8194429089:AAGZst6Z6zr0IncIpx5v0K7U3LSWy7f_RjQ';
+    const chatId = '5148446547';
+    const message = 'Користувач натиснув кнопку "Підтвердити Замовлення в Messenger"';
+
+    fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log('Відповідь Telegram:', data);
+      })
+      .catch(error => {
+        console.error('Помилка:', error);
+      });
+  });
