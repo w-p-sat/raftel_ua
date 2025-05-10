a0f90ba7ef1f8d19bcd286cdafcfdb447d1a5452
Ruzke <146598189+w-p-sat@users.noreply.github.com>
2025-05-10 12:49:35 +0300

    Update buy_bliad.js

--- js/buy_bliad.js
+++ js/buy_bliad.js
@@ -1,7 +1,7 @@
 document.getElementById('buy_bliad').addEventListener('click', () => {
     const botToken = '8194429089:AAGZst6Z6zr0IncIpx5v0K7U3LSWy7f_RjQ';
     const chatId = '5148446547';
-    const message = 'Користувач натиснув кнопку "Підтвердити Замовлення в Messenger"';
+    const message = 'Користувач натиснув кнопку "Купити нажали"';
 
     fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}`)
       .then(response => response.json())
